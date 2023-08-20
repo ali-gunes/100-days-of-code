bbd61a415d54d2e4e369a6fbdf21ed527f5d619d
Ali Güneş <agunes372@gmail.com>
2023-08-20 15:46:41 +0300

    Finished project day 6

---
+++ src/day-6.py
@@ -0,0 +1,24 @@
+# On day 6, we are: Studying Functions and While Loops
+# For Python built-in functions: docs.python.org/3/library/functions.html
+
+# Project 6 - Escaping the Maze
+# Today's project is done on Reeborg's World challenge called "Maze", find it here:
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+# Also completed: Hurdle 1, Hurdle 2, Hurdle 3, Hurdle 4
+
+# I am writing the solution code below:
+"""
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+     if right_is_clear():
+        turn_right()
+        move()
+     elif front_is_clear():
+        move()
+     else:
+        turn_left()
+"""
