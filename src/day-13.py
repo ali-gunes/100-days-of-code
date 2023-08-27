2f1f50f77a18a64b334e9f887d1a7dce9c8e6c1d
Ali Güneş <agunes372@gmail.com>
2023-08-27 23:25:22 +0300

    Finished day 13

---
+++ src/day-13.py
@@ -0,0 +1,13 @@
+# On day 13, we are: Learning Debugging and How to Find Errors in Code
+
+# Everyone gets bugs, we create them
+# Tips for Debugging
+# 1- Describe the problem and test your assumptions
+# 2- Reproduce he bug
+# 3- Play computer and evaluate each line
+# 4- Fix errors and watch underlines
+# 5- Squash bugs with print() statements
+# 6- Bring out the big gun... Use a DEBUGGER
+# 7- Run your code often
+# 8- Rest and step away from the code for a while and try again with fresh eyes
+# 9- Know how to research your problem and use various sources
